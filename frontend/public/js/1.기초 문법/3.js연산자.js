de4a4416dd2ad82d0bce1df2e43504631cd237d6
Ga0Kwon <kgayeong39@gmail.com>
2023-01-09 18:00:34 +0900

    DAY7 수업 기록

--- frontend/public/js/1.기초 문법/3.js연산자.js
+++ frontend/public/js/1.기초 문법/3.js연산자.js
@@ -35,7 +35,7 @@
 																나이 <= 60 ? '중년' : '노년'
  */
 
- //1. 산술 연산자
+/* //1. 산술 연산자
 console.log(3+3) //숫자 + 숫자 => 숫자 
 console.log('더하기 : ' +3 + 3 ); //문자 + 숫자 => 문자 + 숫자 => 문자
 console.log('더하기 :' +(3+3)); //[괄호] 숫자 + 숫자 => 숫자 + 문자 => 문자
@@ -79,3 +79,79 @@ console.log(" %= 대입 후 : " + data1)
 let html = '<h3> JS 작성된 코드 </h3>' //html 마크업 저장할 변수 선언
 html += '<div> 안녕하세요 </div>'  //변수에 마크업 추가
 document.write(html); //해당 변수를 html 출력
+
+//5. 증감 연산자
+let data2 = 10;
+console.log("변수 값 : " + data2) //10
+console.log("변수++ : " + (data2++) ) //10 : 출력 후 증가
+console.log("변수 값 : " + data2)  //11
+console.log("++변수 : " + ++(data2++)) //12 : 출력 전 증가
+
+console.log("변수-- :" + (data2--)) //12 : 출력 후 감소
+console.log("변수 값 : " + data2) //11
+console.log("--변수" + (--data2)) //10 : 출력 전 감소
+console.log("변수 값 : " + data2) //10
+
+//6. 삼항 연산자 [조건 ? 참 : 거짓]
+let age = 38;
+let 나이대 = age <= 19 ? '청소년' : '성인' // 조건이 false니까 '성인' 대입
+console.log("38의 나이대 : " + 나이대)
+
+let 점수 = 78;
+let 등급 = 점수 >= 90 ? 'A등급' : 점수 >= 80 ? 'B등급' : 점수>=70 ? 'C등급' : '탈락'
+
+console.log('78점수의 등급 : ' + 등급);
+*/
+
+/*
+	문제9 : 정수를 입력받아서 '홀수'인지 '짝수'출력
+	문제 10 : 점수 2개를 받아 더 큰 수를 출력
+	문제 11 : 전수 3개를 입력받아 가장 큰 수를 출력
+	문제 12 : 정수 3개를 입력받아 오름차순으로 출력
+
+ */
+// 문제 9
+let number9 =   prompt('숫자를 입력해주세요.');
+let result9 = (number9%2==0) ? '짝수' : '홀수'
+console.log("문제 9> 정수 : " + result9)
+
+//문제 10
+let number10_1 = prompt("숫자를 입력해주세요");
+let number10_2 = prompt("숫자를 입력해주세요");
+let result10 = (number10_1>number10_2) ? number10_1 : (number10_2>number10_1) ? number10_2 : '두수는 같다';
+
+console.log("문제 10> 큰수 : " + result10)
+
+//문제 11
+let number11_1 = prompt("숫자를 입력해주세요.");
+let number11_2 = prompt("숫자를 입력해주세요.");
+let number11_3 = prompt("숫자를 입력해주세요.");
+
+let max = number11_1;
+max = max < number11_2 ? number11_2 : max;
+max = max < number11_3 ? number11_3 : max;
+
+//문제 12
+let number12_1 = prompt("숫자를 입력해주세요.");
+let number12_2 = prompt("숫자를 입력해주세요.");
+let number12_3 = prompt("숫자를 입력해주세요.");
+
+let temp1 = number12_1;
+
+number12_1 > number12_2 ? (temp1 = number12_2) : number12_2 = temp1;
+//number12_1이 number12_2보다 크면 temp1에 number12_1이 들어가있음
+
+number12_2 > number12_3 ? (temp1 = number12_3) : number12_3 = temp1;
+
+number12_1 > number12_3 ? (temp1 = number12_1) : number12_1 = temp1;
+
+
+
+
+
+
+
+
+
+
+
