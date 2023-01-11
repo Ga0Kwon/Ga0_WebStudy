5309b52f6e0facd2a77351a6e011ad2ad68d2e43
Ga0Kwon <kgayeong39@gmail.com>
2023-01-11 15:46:59 +0900

    Day9 강의 정리

--- frontend/public/js/1.기초 문법/9.js반복문연습.js
+++ frontend/public/js/1.기초 문법/9.js반복문연습.js
@@ -1,11 +1,11 @@
 //예1) 1부터 10이하까지 1씩 증가 반복[한줄씩 출력]
-console.log("-------------------------예1-------------------------");
+console.log("---------------------예1---------------------");
 for(let i = 0; i <= 10; i++){ //for start
 	console.log(i);
 } //for end
 
 //예2) i는 1부터 10이하까지 1씩 증가 반복하면서 [한줄에 모두 출력]
-console.log("-------------------------예2-------------------------");
+console.log("---------------------예2---------------------");
 let output = ' ' //문자 변수로 받을려고 공백[깡통] 넣음 -> 이거 안넣으면 진짜 숫자 더하기가 되버림.
 for(let i = 1; i <= 10; i++){ //for start
 	output += i + " " // vs. output = output + i; -> 누적 기록
@@ -13,7 +13,7 @@ for(let i = 1; i <= 10; i++){ //for start
 console.log(output);
 
 //예3) i를 1부터 10이하까지 1씩 증가 반복 [html 출력]
-console.log("-------------------------예3-------------------------");
+console.log("---------------------예3---------------------");
 output = ' ' //위에서 사용했기때문에 공백으로 다시 초기화[깡통]
 for(let i = 1; i <= 10; i++){//for start
 	output += i + "\t"
@@ -21,14 +21,15 @@ for(let i = 1; i <= 10; i++){//for start
 document.querySelector('body').innerHTML = output;
 
 //예4) i는 1부터 10이하까지 1씩 증가 반복 누계
-console.log("-------------------------예4-------------------------");
+console.log("---------------------예4---------------------");
 let sum = 0 ; //숫자 변수
 for(let i = 1; i <= 10; i++){ //for start
 	sum += i
+	console.log("i = " + i + "\t" + i + "<= 10 " + (i<=10));
 } //for end
 console.log(sum);
 
-console.log("-------------------------예5-------------------------");
+console.log("---------------------예5---------------------");
 //예5) i는 1부터 100까지의 7배수 누적 합계
 //1>
 sum = 0;
@@ -42,6 +43,31 @@ sum = 0;
 for(let i = 1; i<=100; i++){ //for start
 	if((i%7) == 0){ //if start
 		sum += i;
+		console.log("i = " + i +"\t" + i + "<= 10 " + (i<=100));
 	} //if end
 } //for end
 console.log(sum);
+
+//예6) (2단)구구단
+console.log("---------------------예6---------------------");
+output = ' ';
+for(let i = 1; i <10; i++){
+	console.log("2 x " + i + " = " + 2*i)
+	output += "2 x " + i + " = " + (2*i) + "</br>"; //누적 기록
+}//for end
+
+document.querySelector('body').innerHTML = output;
+
+//예7) 구구단 전체
+output = ' ';
+console.log("---------------------예7---------------------");
+for(let i = 2; i < 10; i++){
+	console.log("--------------"+ i + "단-----------------")
+	for(let j = 2; j < 10; j++){
+		console.log(i + " x " + j + " = " + i*j);
+		output += i + " x " + j + " = " + (i*j)+"</br>";
+	}
+	console.log("----------------------------------")
+	document.querySelector('body').innerHTML = output;
+	
+}
