fe8400e4998534e1d056947fb3afe31779cfe6c2
Ga0Kwon <kgayeong39@gmail.com>
2023-01-27 17:59:56 +0900

    DAY18 게임

---
+++ frontend/public/js/js과제/과제6/index.js
@@ -0,0 +1,312 @@
+//1. userBox, logBox, monBox Dom객체를 가져오기
+let userBox = document.querySelector('.userBox');
+let monBox =document.querySelector('.monBox');
+
+//로그 출력하는 박스 가져오기.
+let logBox =document.querySelector('.logBox');
+let logBox2 = document.querySelector('.logBox2');
+
+let wrap = document.querySelector('.wrap');
+
+//*캐릭터 객체
+let character = {
+	img : 'img/user/user1.png',
+	left : 10 ,
+	level : 1,
+	exp: 0, 
+	power : 20,
+	hp : 200
+}
+
+//*몬스터 배열
+let monsterArrays = [
+	{name : '마법사', m_img :'img/monster1/monster1.png', hp : 100, left : 910, exp : 50},
+	{name : '빨간용', m_img :'img/monster2/monster2.png', hp : 150, left : 910, exp : 100},
+	{name : '파란용', m_img :'img/monster3/monster3.png', hp : 200, left : 900, exp : 150}
+]
+
+// *userBox의 [기본/처음] 위치
+let u_left = 10; //유저
+
+// *monBox의 [기본/처음] 위치
+let m_left = 910; //몬스터
+
+/*몬스터와 사용자의 HP칸 */
+let monsterMaxHp = 350;
+let userMaxHP = 350;
+
+//2. 문서 안에서 키 입력 이벤트 -> 문서 안에서 키를 입력하는 것이기 때문에 document
+document.addEventListener('keydown', (e)=>{
+/*	console.log('키입력')*/
+/*	console.log(e)*/
+	console.log(e.keyCode)
+	
+	let key = e.keyCode; //입력된 키 코드를 변수에 저장
+	mon_moving(key)
+	
+	if(key == 37){ //왼쪽 키
+		u_left -= 10;
+		u_left = u_left < 0 ? 0 : u_left;
+		userBox.style.backgroundImage = `url(img/user/user_left1.png)`
+	}else if(key == 39){//오른쪽 키
+		u_left += 10;
+		u_left = u_left > 910 ? 910 : u_left;
+		userBox.style.backgroundImage = `url(img/user/user_right1.png)`
+	}else if(key == 65){ //a키 -> 공격
+		Attack();
+		userBox.style.backgroundImage = `url(img/user/user_akey.png)`
+		
+	}else if(key == 32){ //space키 -> 점프
+		userBox.style.backgroundImage = `url(img/user/user_jump.png)`
+		userBox.style.top = `450px`
+	}else if(key == 40){ //아래키 -> 납작하게
+		userBox.style.backgroundImage = `url(img/user/user_down.png)`
+	}else if(key == 83){ //s키 -> 실드
+		userBox.style.backgroundImage = `url(img/user/user_skey.png)`
+	}
+	
+	userBox.style.left = `${u_left}px`
+	
+	//*현재 좌표 로그 출력
+	logBox.innerHTML = `사용자 좌표 : ${u_left}`
+})
+
+//2. 문서 안에서 키를 떼었을 때 이벤트
+document.addEventListener('keyup', (e) => {
+	userBox.style.backgroundImage = `url(img/user/user1.png)`
+	userBox.style.backgroundSize =`90%`;
+	
+	/*console.log(e.keyCode)*/
+	
+	if(e.keyCode == 32){ /*점프를 누르고 놓았을 때 제자리로 내려와야한다. */
+		userBox.style.top = `510px`
+	}
+	
+})
+
+//3. 몬스터 이동 난수 [랜덤 -> 1초]
+//특정시간마다 함수 실행해주는 함수 setInterval(() => {}, 밀리초(1000/1))
+//몬스터가 움직였을 때 사용자와 닿으면 사용자는 데미지를 받는다. [단, 사용자가 공격할때, 사용자가 방어를 할때 제외]]
+setInterval(mon_moving, 1000)
+function mon_moving(key){
+	
+	if(nextMonster() == 3){ //모든 몬스터가 죽은 경우 
+		monBox.style.backgroundImage =`none`
+	}
+	//1. 난수 +- 20
+	let rand = parseInt(Math.random()*35+1); //1~20사이의 랜덤한 정수 //이동 거리
+	let rand2 = parseInt(Math.random()*2); // 0 또는 1 //이동 방향
+	
+	if(rand2  == 1){m_left += rand;}
+	else {m_left -= rand; } 
+	
+	/*사용자 데미지 적용 코드 */
+	for(let i = 0; i < 5; i++){ //+- 5정도
+		
+		if(m_left + i == u_left){ //+5정도 감안하고 겹칠 경우
+			if(nextMonster() == 0){ //첫번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+				
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+					
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster1/monster1_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+				}
+			}else if(nextMonster() == 1){ //두번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+				
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+				
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster2/monster2_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+				}
+			}else if(nextMonster() == 2){ //세번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+				
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+					
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster3/monster3_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+				}
+				
+			}
+			
+		}else if(m_left - i == u_left){//-5정도 감안하고 겹칠 경우
+			if(nextMonster() == 0){ //첫번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+					
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+					
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster1/monster1_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+						document.querySelector('.userBlood').style.width = `${userMaxHP -(userMaxHP/character.hp)*20+1}px`
+					characterDie() //공격이 들어갈때 캐릭터의 hp가 0인지 확인
+				}
+			}else if(nextMonster() == 1){ //두번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+				
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+				
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster2/monster2_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+					document.querySelector('.userBlood').style.width = `${userMaxHP -(userMaxHP/character.hp)*20+1}px`
+					characterDie() //공격이 들어갈때 캐릭터의 hp가 0인지 확인
+				}
+			}else if(nextMonster() == 2){ //세번째 몬스터일 경우
+				if(key == 65){ //사용자가 공격을 했을 경우
+				
+				}else if(key == 83){ //사용자가 실드를 했을 경우
+					
+				}else{ //몬스터의 공격으로 데미지를 받았을 경우
+					monBox.style.backgroundImage = `url(img/monster3/monster3_attack.png)`
+					character.hp -= 10; //몬스터의 공격으로 hp -10
+						document.querySelector('.userBlood').style.width = `${userMaxHP -(userMaxHP/character.hp)*20+1}px`
+					characterDie() //공격이 들어갈때 캐릭터의 hp가 0인지 확인
+					
+				}
+				
+			}
+		
+		}else{
+			if(nextMonster() == 0){
+				monBox.style.backgroundImage = `url(img/monster1/monster1.png)`
+			}else if(nextMonster() == 1){
+				monBox.style.backgroundImage = `url(img/monster2/monster2.png)`
+			}else if(nextMonster() == 2){
+				monBox.style.backgroundImage = `url(img/monster3/monster3.png)`
+			}
+		}
+		
+		
+	}
+	
+	//2.게임화면 고정
+	if(m_left < 0) m_left = 0;
+	if(m_left > 910) m_left = 910;
+	
+	//3.
+	monBox.style.left = `${m_left}px`
+	
+	//*현재 좌표 로그 출력
+	logBox2.innerHTML = `몬스터 좌표 : ${m_left}`
+}
+
+//몬스터가 어떤 몬스터인지 판별하는 함수
+function nextMonster(){
+	let i = 0;
+	
+	if(monsterArrays[0].hp <= 0){
+		monBox.style.backgroundImage = `url(img/monster2/monster2.png)`
+		document.querySelector('.dieMonster1').style.display = "inline"
+		i = 1 //첫번째 몬스터까지 이겼을 경우
+	}
+	if(monsterArrays[1].hp <= 0){
+		monBox.style.backgroundImage = `url(img/monster3/monster3.png)`
+		document.querySelector('.dieMonster2').style.display = "inline"
+		i = 2 //두번째 몬스터까지 이겼을 경우
+	}
+	if(monsterArrays[2].hp <= 0){
+		wrap.style.filter = `brightness(60%)`;
+		wrap.style.textAlign = "center"
+		wrap.style.margin = "0 auto"
+		wrap.style.color = "white"
+		wrap.style.fontSize = "50px"
+		wrap.innerHTML = `<h1>Winner!</h1>`
+		document.querySelector('.dieMonster3').style.display = "inline"
+		i =3 //전부 이겼을 경우
+		printTable();
+	}
+	
+	return i;
+}
+
+/*사용자 공격 */
+function Attack(){ //공격버튼을 눌렀을 때
+	
+	if(nextMonster() != 3){ //아직 이기지 못한 상대가 있을 경우
+		for(let i = 0; i < 15; i++){ //+=10정도는 타격한 것으로 하기 위해 설정
+			if(u_left + i == m_left){ //유저 위치에서 +0~10 값이 몬스터 위치와 같을 때
+				/*alert('타격');*/
+				monsterArrays[nextMonster()].hp -= 20; //해당 몬스터 판별 함수에서 몬스터 인덱스값을 받아서 -20	
+				document.querySelector('.monBlood').style.width = `${monsterMaxHp -(monsterMaxHp/monsterArrays[nextMonster()].hp)*20+1}px`
+			}else if(u_left - i == m_left){ //유저 위치에서 -0~10 값이 몬스터 위치와 같을 때
+				/*alert('타격')*/
+				monsterArrays[nextMonster()].hp -= 20; //해당 몬스터 판별 함수에서 몬스터 인덱스값을 받아서 -20	
+				document.querySelector('.monBlood').style.width = `${monsterMaxHp -(monsterMaxHp/monsterArrays[nextMonster()].hp)*20+1}px`
+			}
+		}
+	}else{
+		return;
+	}
+
+ }
+
+/*사용자 캐릭터가 죽었을때 실행되는 코드*/
+function characterDie(){
+	if(character.hp <= 0){ //캐릭터의 hp가 0보다 작거나 같으면
+		wrap.style.filter = `brightness(60%)`;
+		wrap.style.textAlign = "center"
+		wrap.style.margin = "0 auto"
+		wrap.style.color = "red"
+		wrap.style.fontSize = "50px"
+		wrap.innerHTML = `<h1>GAME OVER</h1>`
+		
+		document.querySelector('.dieCharacter').style.display = "inline"
+		printTable();
+	}
+	return 0;
+}
+/*게임이 끝났을 때 경험치와 레벨 보여줌. [1.내가 졌을 경우/ 2. 모두 이겼을 경우] */
+
+function printTable(){
+	
+	for(let i = 0; i < monsterArrays.length; i++){
+		if(monsterArrays[i].hp <= 0){
+			character.exp += monsterArrays[i].exp;
+		}
+	}
+	if(character.exp >= 100){
+		character.level = parseInt(character.exp/100) + 1;
+	}
+	
+	let html = `<tr>
+					<th>Monster</th>
+					<th>Kill</th>
+					<th>Exp</th>
+					<th>Totalexp</th>
+					<th>Level</th>
+				</tr>`
+	
+	
+	for(let i = 0; i < monsterArrays.length; i++){
+		html += `<tr>
+					<td>${monsterArrays[i].name}</td>
+				 	<td>${monsterArrays[i].hp <= 0 ? "O" : "X" }</td>
+				 	<td>${monsterArrays[i].exp}</td>
+				 	<td>${character.exp}</td>
+				 	<td>${character.level}</td>
+				 </tr>`
+	}
+	
+	document.querySelector('.gameInfo_table').innerHTML = html;
+	return;
+	
+}
+/*
+	함수 형태
+		1. 일반함수 : function 함수명(){ }
+		2. 익명함수 : function(){ }
+		3. 람다식함수 : ( ) => { }
+		4. 변수함수 : let 변수명 = ( ) => { }
+	 Math.random()
+	 	 Math.random() : 0 ~ 1 사이의 실수
+	 	  Math.random()*10 : 0 ~ 10(10은 안나옴)사이의 실수
+
+
+
+*/
