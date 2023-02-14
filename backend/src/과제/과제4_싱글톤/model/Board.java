fe1ce5b18b9587110796e6491464d0451b35a6bc
Ga0Kwon <kgayeong39@gmail.com>
2023-02-14 15:03:52 +0900

    DAY30 순환참조개념 정리

--- backend/src/과제/과제4_싱글톤/model/Board.java
+++ backend/src/과제/과제4_싱글톤/model/Board.java
@@ -4,6 +4,25 @@ package 과제.과제4_싱글톤.model;
  	model 관계
  		1. 한명의 회원이 글 작성한다.		[ object ]
  		2. 회원은 여러개의 글을 작성할수 있다.[ ArrayList ]
+ 		* 양방향 설정 [*toString메소드 사용 불가] : stackOverFlow
+ 			- 객체호출시 -> 주소값
+ 			- 스택 영역              vs.               힙영역
+ 			Board board            =             new Baord();
+ 										           32번지
+ 			Member member          =             new Member();
+ 												   33번지
+		   글쓰기 할때 양방향 설정
+		   		board.setMember(member); //32번지에 33번지를 대입
+		   		member.setBoard(board); //33번지에 32번지를 대입
+		   		
+		   		board 호출시 32번지가 가지고 있던 member 33번지 호출
+		   		33번지가 가지고 있던 32번지를 다시 호출 -> 
+		   		32번지가 33번지를 가지고 있던 33번지.....--->
+		   		=> 이런 경우 stackOverFlow가 생김
+		   		=> 따라서, 주소를 호출하지말고 주소의 필드값을 출력해야 순환참조문제가 걸리지X
+		   
+			System.out.println(board);
+			//스택[변수] 가지고 있는 주소 출력
  */
 
 public class Board {
