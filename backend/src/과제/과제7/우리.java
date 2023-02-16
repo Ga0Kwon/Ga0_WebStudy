7124938dabb6807f695d405f080a132239f6add2
Ga0Kwon <kgayeong39@gmail.com>
2023-02-16 09:37:28 +0900

    과제7 완료 2023-02-16

--- backend/src/과제/과제7/우리.java
+++ backend/src/과제/과제7/우리.java
@@ -5,7 +5,7 @@ public class 우리 extends 은행{
 
 	public 우리(String name, String pw) {
 		super(code, pw, name, 0);
-		System.out.println("신한은행 계좌 만들어주셔서 감사합니다.." );
+		System.out.println("우리은행 계좌 만들어주셔서 감사합니다.." );
 	}
 
 }
