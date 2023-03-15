faf06896aadf5e1182994f6434fd4185ddf605f2
Ga0Kwon <kgayeong39@gmail.com>
2023-03-15 17:22:46 +0900

    Day50 첨부파일 다운로드

--- jspWeb/src/main/java/controller/board/FileDownload.java
+++ jspWeb/src/main/java/controller/board/FileDownload.java
@@ -1,6 +1,12 @@
 package controller.board;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +23,53 @@ public class FileDownload extends HttpServlet {
     
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		//1. 다운로드 할 파일명 요청
+		request.setCharacterEncoding("UTF-8");
+		String bfile = request.getParameter("bfile");
+//		System.out.println("bfile : " + bfile );
+		
+		//2. 파일 다운로드
+			//1) 다운로드할 폴더의 경로 찾기
+//		String path = request.getSession().getServletContext().getRealPath("/board/bfile");
+		
+			//2) 다운로드할 파일의 경로 찾기
+		String path = request.getSession().getServletContext().getRealPath("/board/bfile/" + bfile);
+		//System.out.println("path : " + path);
+		
+		//3. 파일 클래스 [해당 경로의 파일을 파일 객체화]
+		File file = new File(path); //해당 경로의 파일을 객체화
+		
+		//3-1. HTTP 다운로드 형식[프론트엔드] = 각 브라우저 [크롬(아래), 엣지(오른쪽 위)]제공
+			// 이게 없으면 다운로드가 아닌 이미지를 띄워주며 경로를 이동한다
+		response.setHeader( // HTTP Header 메소드[HTTP 옵션정보 설정]
+				"Content-Disposition",  // 각 브라우저마다 다운로드형식 HTTP 옵션에 포함해서 보내기
+				"attachment;filename="+URLEncoder.encode(bfile, "UTF-8") //한글이 들어갈 경우
+				// 다운로드시 파일명이 표시되는 옵션 => 한글도 가능하도록![이 코드 빼면 한글 깨짐] => 한글 파일 인코딩
+				// URLEncoder.encode(파일명, "UTF-8"): URL 통신(통신주소)의 한글 인코딩
+				);
+		
+		//4. 파일 스트림[바이트단위] * 예외 발생
+			//1) 파일 객체의 바이트를 모두 읽어온다.
+				//1> 해당 경로[파일] 파일입력스트림객체 만들기
+		BufferedInputStream fin = new BufferedInputStream(new FileInputStream(file));
+				//2> 파일입력스트림객체에서 꺼내올 바이트들을 저장할 바이트배열 준비[선언]
+				// 바이트 배열 길이 = 파일의 길이[바이트길이]
+		byte[] bytes = new byte[(int)file.length()];
+		
+				//3> 파일입력스트림객체에서 read() : 해당 파일을 바이트로 읽어오는 함수
+		fin.read(bytes);
+			//2) 읽어온 바이트를 모두 출력한다. [클라이언트에게 응답 response]
+				//1> response 응답 객체에서 출력 스트림 호출해서 파일출력스트림객체 만들기
+				//response.getOutputStream() : HTTP 스트림 단위[바이트] 전송
+		BufferedOutputStream fout = new BufferedOutputStream(response.getOutputStream());
+				//2> 파일 입력스트림 객체에서 읽어온 바이트들[바이트배열]을 내보냄
+				// 파일출력스트림객체.write() : 해당 배열내 바이트를 출력하는 함수
+		fout.write(bytes);
+			
+			//3) 스트림 닫기 [JVM] => CG가 알아서 닫고 삭제 하겠지만 대용량이기 때문에 직접 닫는게 좋다.
+		fin.close(); //파일입력스트림 객체 닫기[초기화]
+		fout.flush(); //파일출력스트림객체 스트림 메모리 초기화
+		fout.close(); // 파일출력스트림객체 닫기
 	}
 
 
--- jspWeb/src/main/webapp/js/board/view.js
+++ jspWeb/src/main/webapp/js/board/view.js
@@ -5,14 +5,12 @@ getBoard()
 function getBoard(){
 	//bno가져오기
 	let bno = document.querySelector('.bno').innerHTML;
-	console.log("bno : "+bno);	
 	
 	$.ajax({
 		url : "/jspWeb/board/info",
 		method : "get",
 		data : {"type" : 2, "bno" : bno}, //1. 전체 출력 2. 개별 출력
 		success : (r) => {
-			console.log(r)
 			if(r != null){
 				document.querySelector('.infoBox').innerHTML = `
 				${r.bwritedate} | ${r.bview} | ${r.blike} | ${r.bhate} `;
@@ -34,11 +32,27 @@ function getBoard(){
 		}
 	})
 }
+/*
+	1. onClick = JS 코드 작성 구역
+	
+		<button onClick = "bfileDownload(${r.bfile})"
+			짱구4.jpg [문자처리를 안하면 .연산자가 실행되가지고, 에러가 난다.]
+		<button onClick = "bfileDownload('${r.bfile}')"
+		 '짱구4.jpg' -> 작은따옴표나 통해 문자열로 처리해준다. [큰따옴표를 이미 쓰고 있으므로.. 작은따옴표]
+
+	2. 전송 방법
+		HTML : 1. <form> 2. <a href = "">
+		JS : 1. location.href = ""
+		JQUERY : 1.$.ajax({})
+		servlet : 1. response.getWriter.print(문자데이터);
+				  2. response.sendRedirect('경로');
+				  
+*/
 
 //첨부파일 다운로드
 function bfileDownload(bfile){
 	
-	$.ajax({
+	/*$.ajax({
 		url : "/jspWeb/filedownload",
 		method : "get",
 		data : {"bfile" : bfile},
@@ -46,15 +60,11 @@ function bfileDownload(bfile){
 			console.log(r);
 			
 		}
-	})
+	})*/
+	
+	location.href = "/jspWeb/filedownload?bfile=" + bfile;
+	
 }
-
-/*
-	<button onClick = "bfileDownload(${r.bfile})"
-		짱구4.jpg [문자처리를 안하면 .연산자가 실행되가지고, 에러가 난다.]
-	<button onClick = "bfileDownload('${r.bfile}')"
-	 '짱구4.jpg' -> 작은따옴표나 통해 문자열로 처리해준다. [큰따옴표를 이미 쓰고 있으므로.. 작은따옴표]
-*/
 
 //게시물 작성자의 정보를 확인하기[인수 mno]
 function friendInfo(mno){
