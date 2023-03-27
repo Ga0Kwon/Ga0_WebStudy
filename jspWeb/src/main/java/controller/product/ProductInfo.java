e963efbef5543d02ac6b1881001df8000b182841
Ga0Kwon <kgayeong39@gmail.com>
2023-03-27 11:34:40 +0900

    DAY58 commons 파일 업로드 사용하기

--- jspWeb/src/main/java/controller/product/ProductInfo.java
+++ jspWeb/src/main/java/controller/product/ProductInfo.java
@@ -1,14 +1,20 @@
 package controller.product;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oreilly.servlet.MultipartRequest;
@@ -23,28 +29,28 @@ import model.dto.ProductDto;
 @WebServlet("/product/info")
 public class ProductInfo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProductInfo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    ObjectMapper mapper = new ObjectMapper();
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProductInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	ObjectMapper mapper = new ObjectMapper();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
-		String 동 = request.getParameter("동");	System.out.println(동);
-		String 서 = request.getParameter("서");	System.out.println(서);
-		String 남 = request.getParameter("남");	System.out.println(남);
-		String 북 = request.getParameter("북");	System.out.println(북);
-		
-		
-		ArrayList<ProductDto> result = ProductDao.getInstance().getProductList( 동 , 서 , 남 , 북 );
-		
+		String 동 = request.getParameter("동");
+		String 서 = request.getParameter("서");
+		String 남 = request.getParameter("남");
+		String 북 = request.getParameter("북");
+
+		ArrayList<ProductDto> result = ProductDao.getInstance().getProductList(동, 서, 남, 북);
+
 		String jsonarray = mapper.writeValueAsString(result);
 		System.out.println(result);
 		response.setCharacterEncoding("UTF-8");
@@ -53,25 +59,79 @@ public class ProductInfo extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*-------------------------- commons.jar 사용시 --------------------------*/
+		request.setCharacterEncoding("UTF-8");
 		
+		//1. 다운로드 할 서버 경로
 		String path = request.getSession().getServletContext().getRealPath("/product/pimg");
 		
-		MultipartRequest multi = new MultipartRequest(
-				request, path , 1024*1024*10 , "UTF-8" , new DefaultFileRenamePolicy() );
+		//2. 해당 경로의 파일/폴더 객체화 [setRepository에서 대입하기 위해 ] 저장 경로 객체(savePath)
+		File savePath = new File(path);
 		
-		String pname = multi.getParameter("pname");						System.out.println( pname );
-		String pcomment = multi.getParameter("pcomment");				System.out.println( pcomment );
-		int pprice = Integer.parseInt( multi.getParameter("pprice") );	System.out.println( pprice );
-		String plat = multi.getParameter("plat");						System.out.println( plat );
-		String plng = multi.getParameter("plng");	
-		System.out.println( plng );
+		//3.업로드할 저장소 객체 생성
+		DiskFileItemFactory saveStore = new DiskFileItemFactory();
+		saveStore.setRepository(savePath); //파일 저장소 위치 대입
+		saveStore.setSizeThreshold(1024*1024*10); //파일 저장소에 저장할 수 있는 최대용향 범위
 		
-		ProductDto dto  = new ProductDto(pname, pcomment, pprice, plat, plng);	System.out.println( dto );
-		boolean result = ProductDao.getInstance().write(dto);
-		response.getWriter().print(result);
+		//4. 파일 업로드 객체
+		ServletFileUpload fileupload = new  ServletFileUpload(saveStore);
+		
+		try {
+			//5. 매개변수 요청해서 리스트에 담기 [무조건 예외처리 발생] 
+			List<FileItem> fileitemList = fileupload.parseRequest(request);
+			
+			//6.
+			for(FileItem item : fileitemList) {//요청된 모든 매개변수들을 반복문 돌려서 확인
+				System.out.println("필드명 : " + item.getFieldName());  //매개변수명 확인
+				System.out.println("필드내 첨부된 파일명" + item.getName()); //매개변수가 파일일 경우 파일명 확인
+
+			}
+			
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
+		/*-------------------------- cos.jar 사용시 --------------------------*/
+		/*
+		 * String path =
+		 * request.getSession().getServletContext().getRealPath("/product/pimg");
+		 * 
+		 * MultipartRequest multi = new MultipartRequest(request, path, 1024 * 1024 *
+		 * 10, "UTF-8", new DefaultFileRenamePolicy());
+		 * 
+		 * String pname = multi.getParameter("pname"); String pcomment =
+		 * multi.getParameter("pcomment"); int pprice =
+		 * Integer.parseInt(multi.getParameter("pprice")); String plat =
+		 * multi.getParameter("plat"); String plng = multi.getParameter("plng");
+		 */
+
+		/*
+		 * //첨부파일 1개 이름 가져오기 multiple x String pfile = multi.getFilesystemName("pfile");
+		 * 
+		 * //첨부파일 여러개 이름 가져오기 1 multiple x String pfile1 =
+		 * multi.getFilesystemName("pfile1"); String pfile2 =
+		 * multi.getFilesystemName("pfile2"); String pfile3 =
+		 * multi.getFilesystemName("pfile3");
+		 * 
+		 * //첨부파일 여러개 이름 가져오기 2 multiple //multiple input에 등록된 여러 사진들의 이름을 가져오기 불가능 ->
+		 * [cos.jar 제공X] //다른 라이브러리 사용 String pfiles =
+		 * multi.getFilesystemName("pfiles"); //이름이 하나밖에 안가져옴 Enumeration<String> pfiles
+		 * = multi.getFileNames(); //input type이 file의 태그명 호출
+		 * 
+		 * while(pfiles.hasMoreElements()) { //해당 목록 [pfiles] 에 요소 [element]가 존재하면 true/
+		 * 아니면 false String s = pfiles.nextElement(); //다음요소 가져오기 }
+		 */
+		/*
+		 * ProductDto dto = new ProductDto(pname, pcomment, pprice, plat, plng);
+		 * System.out.println(dto); boolean result =
+		 * ProductDao.getInstance().write(dto); response.getWriter().print(result);
+		 */
 	}
 
 }
--- jspWeb/src/main/webapp/product/write.jsp
+++ jspWeb/src/main/webapp/product/write.jsp
@@ -12,13 +12,27 @@
 	
 	<div class = "container">
 		<form class = "writeform" name = "pname">
-			제품명 : <input type = "text" name = "pname">
-			제품 설명 : <input type = "text" name = "pcomment">
-			제품 가격 : <input type = "text" name = "pprice">
+			제품명 : <input type = "text" name = "pname"><br/>
+			제품 설명 : <input type = "text" name = "pcomment"><br/>
+			제품 가격 : <input type = "text" name = "pprice"><br/>
 	   		거래 위치 : 
 			<div id="map" style="width:100%;height:350px;"></div>
+			
+			<!-- 첨부파일 1개 -->
+			<!-- <div>첨부파일 1개</div>
+			<input type = "file" name = "pfile" accept = "image/*"> image/* : 이미지 파일만 가능 audio넣으면 audio만 -->
+			
+			<!-- 첨부파일 여러개 -->
+			<!-- <div>첨부파일 여러개 []pfile 여러개]</div>
+			<input type = "file" name = "pfile1" accept = "image/*"> image/* : 이미지 파일만 가능 audio넣으면 audio만
+			<input type = "file" name = "pfile2" accept = "image/*"> image/* : 이미지 파일만 가능 audio넣으면 audio만
+			<input type = "file" name = "pfile3" accept = "image/*"> image/* : 이미지 파일만 가능 audio넣으면 audio만 -->
+			
+			<!-- 첨부파일 여러개 -->
+			<div>첨부파일 여러개 [multiple : cos 불가능 / commons 가능]</div>
+			<input type = "file" name = "pfiles" multiple="multiple" accept = "image/*"> <!-- 멀티플의 경우 여러개 한꺼번에 선택 가능 -->
+			<br/>
 			<button onClick = "onwrite()" type = "button">제품 등록</button>
-
 		</form>
 	</div>
 	
